5e71eb083302cde265e145ab546e50603d1e5cc8
Theehawau <hawau.olamide.th@gmail.com>
2021-08-21 14:16:20 +0000

    to get, regions, bounds, url data location

---
+++ .ipynb_checkpoints/visualize-checkpoint.py
@@ -0,0 +1,62 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.interpolate import griddata
+import numpy as np
+def heatmap(shpfile:str, nodata_value:int = -9999)-> None:
+    """
+    Parameters
+    ----------
+    shpfile:str :path to shapefile
+    none_value:int :value used to represent nodata values in point cloud data, defaults to -9999
+    Returns
+    -------
+    None
+    """
+#     read shape file 
+    geo_df = gpd.read_file(shpfile)
+    
+#     remove no data values from data
+    df = geo_df[geo_df['elevation_'] > nodata_value]
+    
+    plt.rcParams["figure.figsize"] = (20,10)
+    fig, ax = plt.subplots(1, 1)
+    df.plot(column='elevation_', cmap='OrRd', ax=ax, legend=True, legend_kwds={'label': "Elevation by point"})
+    ax.legend(fontsize=15)
+    
+def map3d(shpfile:str, nodata_value:int = -9999)-> None:
+    """
+    Parameters
+    ----------
+    shpfile:str :path to shapefile
+    none_value:int :value used to represent nodata values in point cloud data, defaults to -9999
+    Returns
+    -------
+    None
+    """
+    #     read shape file 
+    geo_df = gpd.read_file(shpfile)
+    
+#     remove no data values from data
+    geo_df = geo_df[geo_df['elevation_'] > nodata_value]
+    geo_df['geometry'] = geo_df.geometry.centroid
+    
+    xyz = zip(geo_df.geometry.x,geo_df.geometry.y,geo_df['elevation_'])
+    data = pd.DataFrame(xyz)
+    x = data[0].values
+    y = data[1].values
+    z = data[2].values
+    xi = np.linspace(x.min(),x.max(),100)
+    yi = np.linspace(y.min(),y.max(),100)
+    zi = griddata((x, y), z, (xi[None,:], yi[:,None]), method='nearest')
+    xig, yig = np.meshgrid(xi, yi)
+    
+    plt.rcParams["figure.figsize"] = (20,10)
+    fig = plt.figure()
+    ax = fig.add_subplot(1,2,1, projection='3d')
+    my_plot = ax.plot_surface(xig, yig, zi,rstride=1, cstride=1, cmap='OrRd', linewidth=0, antialiased=True)
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    ax.set_zlabel("Elevation")
+    ax.set_title('Surface Elevation Plot')
+    plt.show()
--- GetBounds.py
+++ GetBounds.py
@@ -2,49 +2,67 @@ import pandas as pd
 import json
 import requests
 import sys
+import csv
 
 class get_bounds_region:
+    """ """
     def __init__(self,filepath:str):
         self.regions_path = filepath
-        self.region_to_location={}
-        self.region_to_bounds={}
-    
-    def get_region_to_location(self):
-        region_to_location = {}
+        self.data = []
+    def get_region_to_location(self) -> None:
+        """ 
+        This functions creates a list of dict in the format:
+        [{'region':'','location':''},...]
+        """
         print('Getting location to region names...')
         with open (self.regions_path, encoding="utf-8") as f:
             f.readline()
             for line in f:
                 filename = line.replace('\n','')
                 location = f'https://s3-us-west-2.amazonaws.com/usgs-lidar-public/{filename}ept.json'
-                region_to_location[filename] = location
-            self.region_to_location = region_to_location
+                dic = {'region':filename,'location':location}
+                self.data.append(dic)
         print('Successful')
             
-    def get_region_bounds(self):
-        file_to_bounds = {}
+    def get_region_bounds(self) -> None:
+        """ 
+        This functions updates the list of dict to:
+        [{'region':'','location':'','bounds':''},...]
+        """
         print('Getting bounds to region names')
-        for file,location in self.region_to_location.items():
+        for dic in self.data:
             try:
-                json_file = requests.get(location).json()
-                file_to_bounds[file] = json_file['bounds']
+                json_file = requests.get(dic['location']).json()
+                bound = json_file['bounds']
+                dic['bounds'] = bound
             except Exception:
-                print(f"Error occured couldn't find bounds for {file}")
+                print(f"Error occured couldn't find bounds for {dic['region']}")
                 continue
-        self.region_to_bounds = file_to_bounds
         print('Successful')
+        
+    def save_data(self, filename:str = 'metadata.csv') -> None:
+        """
+        This function saves the dict as a csv file
+        Parameters
+        ----------
+        filename:str :
+             (Default value = 'metadata.csv')
+             
+        Returns
+        -------
+        None
+        """
+        print('Saving file to metadata.csv ...')
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['region','location','bounds'])
+            writer.writeheader()
+            writer.writerows(self.data)
+        print('File saved successfully')    
+
+        
+if __init__ == '__main__':
+    get = get_bounds_region('filename.txt')
+    get.get_region_to_location()
+    get.get_region_bounds()
+    get.save_data()
     
-    def get_dataframe(self):
-        print('get data for dataframe...')
-        for region,bounds in self.region_to_bounds.items():
-            regions = []
-            bounds = []
-            locations=[]
-            regions.append(region)
-            bounds.append(bounds)
-            locations.append(self.region_to_location[file])
-        self.data = pd.DataFrame({'region':regions,'bounds':bounds,'location':locations})
-        print('Successful')
-    def save_data(self):
-        self.data.to_csv('metadata.csv')
-        print('File saved successfully')
